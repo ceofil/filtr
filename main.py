75c416ae0ec2a47bae0e47e0e44bda9fe2ba75ed
Teofil-Constantin Ciobanu <tciobanu.dev@gmail.com>
2025-04-21 00:17:21 +0300

    mark photos

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import os
 import tkinter as tk
 from PIL import Image, ImageTk
 from dotenv import dotenv_values
+from enum import Enum
+
+class VISITED(Enum):
+    NOT_VISITED = 1
+    VISITED = 2
+    MARKED = 3
 
 config = dotenv_values(".env")
 SOURCE_FOLDER = config['SOURCE_FOLDER']
-TARGET_FOLDER = config['TARGET_FOLDER']
 
 VISITED_JSON = 'visited.json'
 
@@ -26,11 +31,26 @@ class ImageViewer(tk.Tk):
     def __init__(self, image_paths):
         super().__init__()
         self.title("Image Viewer")
-        self.image_paths = image_paths
-        self.idx = 0
-
+        if os.path.exists(VISITED_JSON):
+            print('here')
+            with open(VISITED_JSON, 'r') as fd:
+                self.filter = json.load(fd)
+            self.filter = {k: v for k, v in self.filter.items() if "\\.thumbnails\\" not in k}
+            self.image_paths = list(self.filter.keys())
+            self.idx = 0
+            while self.filter[self.image_paths[self.idx]] != VISITED.NOT_VISITED.value:
+                self.idx += 1
+        else:
+            print("first time running, creating empty filter")
+            self.image_paths = image_paths
+            self.filter = {path: VISITED.NOT_VISITED.value for path in image_paths}
+            self.idx = 0
+        self.marked = 0
+        for k,v in self.filter.items():
+            if v == VISITED.MARKED.value:
+                self.marked += 1
         self.state("zoomed")
-        self.bind("<Escape>", lambda e: self.destroy())  # Press Esc to exit
+        self.bind("<Escape>", self.escape_pressed)
 
         self.update_idletasks()
         self.screen_width = int(self.winfo_screenwidth() * 0.8)
@@ -43,15 +63,28 @@ class ImageViewer(tk.Tk):
         self.status = tk.Label(self, text="", font=("Arial", 16), bg="black", fg="white")
         self.status.pack(side=tk.BOTTOM, fill=tk.X)
 
-        self.configure(bg='lightblue')
+        self.visit()
         self.show_image()
 
         self.bind("<Left>", self.prev_image)
         self.bind("<Right>", self.next_image)
+        self.bind("<space>", self.toggle_mark)
         self.focus_set()
 
+    def dispaly_status(self):
+        path = self.image_paths[self.idx]
+        marked = self.filter[path] == VISITED.MARKED.value
+        marked_txt = "[MARKED]\t" if marked else ""
+        rel_path = os.path.relpath(path, SOURCE_FOLDER)
+        self.status.config(
+            text=f"{marked_txt}\t{self.idx + 1}/{len(self.image_paths)}\t{rel_path} ({self.marked} or {int(self.marked/self.idx*100)}% marked)",
+            fg="lightgreen" if marked else "white"
+        )
+
+
     def show_image(self):
-        image = Image.open(self.image_paths[self.idx])
+        path = self.image_paths[self.idx]
+        image = Image.open(path)
 
         background = Image.new("RGB", self.target_size, (0, 0, 0))
         image.thumbnail(self.target_size, Image.Resampling.LANCZOS)
@@ -61,9 +94,32 @@ class ImageViewer(tk.Tk):
         self.photo = ImageTk.PhotoImage(background)
         self.label.config(image=self.photo)
         self.label.image = self.photo
-        self.status.config(
-            text=f"{self.idx + 1}/{len(self.image_paths)}\t{self.image_paths[self.idx]}"
-        )
+        self.dispaly_status()
+
+    def visit(self):
+        if self.filter[self.image_paths[self.idx]] == VISITED.NOT_VISITED.value:
+            self.filter[self.image_paths[self.idx]] = VISITED.VISITED.value
+    
+    def mark(self):
+        self.filter[self.image_paths[self.idx]] = VISITED.MARKED.value
+        self.next_image(event=None)
+
+    def unmark(self):
+        self.filter[self.image_paths[self.idx]] = VISITED.VISITED.value
+        self.dispaly_status()
+
+    def toggle_mark(self, event):
+        if self.filter[self.image_paths[self.idx]] == VISITED.MARKED.value:
+            self.unmark()
+            self.marked -= 1
+        else:
+            self.mark()
+            self.marked += 1
+
+    def save_filter(self):
+        with open(VISITED_JSON, 'w') as fd:
+            json.dump(self.filter, fd, indent=2)
+            print('saved')
 
     def prev_image(self, event):
         if self.idx > 0:
@@ -73,7 +129,15 @@ class ImageViewer(tk.Tk):
     def next_image(self, event):
         if self.idx < len(self.image_paths) - 1:
             self.idx += 1
+            self.visit()
             self.show_image()
+            if (self.idx + 1) % 100 == 0:
+                self.save_filter()
+
+
+    def escape_pressed(self, event):
+        self.save_filter()
+        self.destroy()
 
 def main():
     all_images = get_all_images(SOURCE_FOLDER)
